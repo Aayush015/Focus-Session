31880e4499ef1eb12373719b22739c13eac6baa7
Aayush015 <aayushkafle308@gmail.com>
2024-08-02 20:35:17 -0600

    Added back button for profile management.

--- app/src/main/java/com/example/focussession/SettingsActivity.java
+++ app/src/main/java/com/example/focussession/SettingsActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -22,6 +23,20 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        // Handle back button
+        Toolbar toolbar = findViewById(R.id.backButton);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         findViewById(R.id.btnSignOut).setOnClickListener(new View.OnClickListener() {
             @Override
